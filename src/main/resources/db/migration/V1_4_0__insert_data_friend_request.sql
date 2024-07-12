daf2d4fa07f5088ba563c9bc403956d89a7fd5a4
Hai Nguyen <hai.nguyen@aeris.net>
2024-07-12 09:11:12 +0700

    BE-6: add sql file

---
+++ src/main/resources/db/migration/V1_4_0__insert_data_friend_request.sql
@@ -0,0 +1,9 @@
+INSERT INTO friend_requests (sender_id, recipient_id, status)
+VALUEs
+    ('4', 'test', 'PENDING'),
+    ('5', 'test', 'PENDING'),
+    ('6', 'test', 'PENDING'),
+    ('7', 'test', 'PENDING'),
+    ('8', 'test', 'PENDING'),
+    ('9', 'test', 'PENDING'),
+    ('10', 'test', 'PENDING');
